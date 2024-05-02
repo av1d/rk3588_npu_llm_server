d91c01e4643e6a0b02124d969b51573f0fc84355
av1d <69888153+av1d@users.noreply.github.com>
2024-05-01 22:15:55 -0400

    Update server.cpp
    cleanup, optimizations, added "status" feature

--- server.cpp
+++ server.cpp
@@ -7,15 +7,11 @@
 #include <cpprest/http_listener.h>
 #include <cpprest/http_msg.h>
 #include <thread>
-#include <string.h> /* rkllm includes begin here */
 #include <unistd.h>
-#include <string>
 #include "rkllm.h"
 #include <fstream>
 #include <sstream>
-#include <iostream>
 #include <csignal>
-#include <vector> /* end rkllm includes */
 
 using namespace web;
 using namespace web::http;
@@ -24,15 +20,13 @@ using namespace utility;
 using namespace std;
 
 std::string PROMPT_TEXT_PREFIX;
+std::string INPUT_STR;
 std::string PROMPT_TEXT_POSTFIX;
 std::string g_llmResponse;
-
 LLMHandle llmHandle = nullptr;
 
-void exit_handler(int signal)
-{
-    if (llmHandle != nullptr)
-    {
+void exitHandler(int signal) {
+    if (llmHandle != nullptr) {
         std::cout << "Caught exit signal. Exiting..." << std::endl;
         LLMHandle _tmp = llmHandle;
         llmHandle = nullptr;
@@ -52,58 +46,70 @@ void callback(const char *text, void *userdata, LLMCallState state) {
     }
 }
 
-// Parse the incoming JSON payload
-void parse_json(const std::string& json_str, std::string& prompt_text_prefix, std::string& input_str, std::string& prompt_text_postfix) {
+void parseJson(const std::string& jsonStr, std::string& promptTextPrefix, std::string& inputStr, std::string& promptTextPostfix) {
     try {
         boost::property_tree::ptree pt;
-        std::stringstream ss(json_str);
+        std::stringstream ss(jsonStr);
         boost::property_tree::read_json(ss, pt);
 
-        prompt_text_prefix = pt.get<std::string>("PROMPT_TEXT_PREFIX");
-        input_str = pt.get<std::string>("input_str");
-        prompt_text_postfix = pt.get<std::string>("PROMPT_TEXT_POSTFIX");
+        promptTextPrefix = pt.get<std::string>("PROMPT_TEXT_PREFIX");
+        inputStr = pt.get<std::string>("input_str");
+        promptTextPostfix = pt.get<std::string>("PROMPT_TEXT_POSTFIX");
     } catch (const std::exception& e) {
         std::cerr << "Error parsing JSON: " << e.what() << std::endl;
     }
 }
 
-void handle_post(http_request message) {
-    auto remote_address = message.remote_address();
-
+void handlePost(http_request message) {
+    auto remoteAddress = message.remote_address();
     auto body = message.extract_string().get();
-    std::string prompt_text_prefix, input_str, prompt_text_postfix;
+    std::string promptTextPrefix, inputStr, promptTextPostfix;
 
-    // Print the incoming IP and JSON request
-    std::cout << remote_address << " - " << body << std::endl;
+    std::cout << remoteAddress << " - " << body << std::endl;
 
-    parse_json(body, prompt_text_prefix, input_str, prompt_text_postfix);
+    parseJson(body, promptTextPrefix, inputStr, promptTextPostfix);
 
-    // Create JSON response with the parsed values
-    json::value response_json;
-    response_json[U("prompt_text_prefix")] = json::value::string(utility::conversions::to_string_t(prompt_text_prefix));
-    response_json[U("input_str")] = json::value::string(utility::conversions::to_string_t(input_str));
-    response_json[U("prompt_text_postfix")] = json::value::string(utility::conversions::to_string_t(prompt_text_postfix));
+    json::value responseJson;
+    responseJson[U("prompt_text_prefix")] = json::value::string(utility::conversions::to_string_t(promptTextPrefix));
+    responseJson[U("input_str")] = json::value::string(utility::conversions::to_string_t(inputStr));
+    responseJson[U("prompt_text_postfix")] = json::value::string(utility::conversions::to_string_t(promptTextPostfix));
 
-    std::string text = response_json[U("prompt_text_prefix")].as_string() +
-                       response_json[U("input_str")].as_string() +
-                       response_json[U("prompt_text_postfix")].as_string();
+    std::string text = responseJson[U("prompt_text_prefix")].as_string() +
+                       responseJson[U("input_str")].as_string() +
+                       responseJson[U("prompt_text_postfix")].as_string();
 
     std::string llmOutput;
-    // Use the callback function to capture the LLM output
     int runResult = rkllm_run(llmHandle, text.c_str(), &llmOutput);
 
     if (runResult != 0) {
-        printf("Error running LLM. Exiting...\n");
+        std::cerr << "Error running LLM." << std::endl;
+        json::value errorJson;
+        errorJson[U("content")] = json::value::string(utility::conversions::to_string_t("Error running LLM."));
+
+        http_response response(status_codes::InternalError);
+        response.set_body(errorJson.serialize());
+        response.headers().add(U("Content-Type"), U("application/json"));
+
+        message.reply(response).wait();
         return;
     }
 
-    // Create a new JSON response with the LLM output
-    json::value output_json;
-    output_json[U("content")] = json::value::string(utility::conversions::to_string_t(llmOutput));
+    json::value outputJson;
+    outputJson[U("content")] = json::value::string(utility::conversions::to_string_t(llmOutput));
 
-    // Prepare HTTP response
     http_response response(status_codes::OK);
-    response.set_body(output_json.serialize());
+    response.set_body(outputJson.serialize());
+    response.headers().add(U("Content-Type"), U("application/json"));
+
+    message.reply(response).wait();
+}
+
+void handleGet(http_request message) {
+    json::value responseJson;
+    responseJson[U("content")] = json::value::string(utility::conversions::to_string_t("online"));
+
+    http_response response(status_codes::OK);
+    response.set_body(responseJson.serialize());
     response.headers().add(U("Content-Type"), U("application/json"));
 
     message.reply(response).wait();
@@ -114,14 +120,15 @@ int main(int argc, char* argv[]) {
         std::cerr << "Usage: " << argv[0] << " <IP> <port> <rkllm_model_path>" << std::endl;
         return 1;
     }
-    std::string rkllm_model_path = argv[3];
 
-    signal(SIGINT, exit_handler);
-    std::string rkllm_model(argv[3]);
-    printf("rkllm init start\n");
+    std::string rkllmModelPath = argv[3];
+    signal(SIGINT, exitHandler);
+    signal(SIGTERM, exitHandler);
+
+    std::cout << "rkllm init start" << std::endl;
 
     RKLLMParam param = rkllm_createDefaultParam();
-    param.modelPath = rkllm_model.c_str();
+    param.modelPath = rkllmModelPath.c_str();
     param.target_platform = "rk3588";
     param.num_npu_core = 2;
     param.top_k = 1;
@@ -130,11 +137,11 @@ int main(int argc, char* argv[]) {
 
     int initResult = rkllm_init(&llmHandle, param, callback);
     if (initResult != 0) {
-        printf("RKLLM init failed!\n");
+        std::cerr << "RKLLM init failed!" << std::endl;
         return -1;
     }
-    printf("RKLLM init success!\n");
 
+    std::cout << "RKLLM init success!" << std::endl;
 
     std::string ip = argv[1];
     std::string port = argv[2];
@@ -142,11 +149,12 @@ int main(int argc, char* argv[]) {
 
     http_listener listener(U(url));
 
-    listener.support(methods::POST, handle_post);
+    listener.support(methods::POST, handlePost);
+    listener.support(methods::GET, handleGet);
 
     try {
         listener.open().wait();
-        std::cout << "\nUsing RKLLM model from: " << rkllm_model_path << std::endl;
+        std::cout << "\nUsing RKLLM model from: " << rkllmModelPath << std::endl;
         std::cout << "Do not use in a production deployment." << std::endl;
         std::cout << "Listening for HTTP requests on " << ip << ":" << port << "..." << std::endl;
 
